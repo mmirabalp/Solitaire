f7c66399da35dbeb8023268d9d5955a5ae4408f2
Mawel Mirabal <mmirabalp@gmail.com>
2018-05-07 01:58:10 -0700

    Project Start

---
+++ src/Card.java
@@ -0,0 +1,16 @@
+public class Card {
+    private int value;
+    private String suit;
+    private boolean faceUp;
+
+    public Card(int value, String suit, boolean faceUp) {
+        this.value = value;
+        this.suit = suit;
+        this.faceUp = faceUp;
+    }
+
+    public int getValue() {
+        return value;
+    }
+}
+
